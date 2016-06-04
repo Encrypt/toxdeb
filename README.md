2d8015be0e8ec448c7c0dd958c40123e4623357e
Yann Privé <encrypt@encrypt-tips.tk>
2016-06-04 18:53:11 +0100

    The script now works on Tox's Jenkins!

--- README.md
+++ README.md
@@ -2,7 +2,10 @@
 
 ToxDeb is a bash script which long-term aim is to build Tox clients for Debian platforms.  
 
-For each client, a configuration should be written and put in the configs/ folder.
-Three configurations are currently available and will hopefully build: [uTox](https://github.com/grayhatter/utox), [qTox](https://github.com/tux3/qTox) and [Ricin](https://github.com/RicinApp/Ricin).
+For each client and for each distribution, a configuration should be written and put in the configs/ folder.
+Various configurations are currently available and will hopefully build: [uTox](https://github.com/grayhatter/utox), [qTox](https://github.com/tux3/qTox) and [Ricin](https://github.com/RicinApp/Ricin).
+
+This script has been adapted to run on [Tox's Jenkins](https://build.tox.chat).
+If you wish to run it on your own server, check out the [initial branch](https://github.com/Encrypt/toxdeb/tree/initial).
 
 It is still under development, released under the GPLv3 licence.
--- toxdeb.sh
+++ toxdeb.sh
@@ -23,143 +23,149 @@ readonly PROGNAME=$(basename $0)
 readonly ARGS=("$@")
 readonly ARGS_NB=$#
 
-# Main function 
+# Main function
 main() {
 	
 	# Local variables
-	local client_version conf_file
-	local i distribution architecture
+	local client_version config i
+	local dist arch config conf_file
 	
 	# Check the number of arguments before going forward
 	[[ ${ARGS_NB} -lt 1 ]] && { error 'argument_missing' ; return $? ; }
 	
+	# Warn if this is run as root
+	[[ $(id -u) -eq 0 ]] && echo "WARNING: That's weird, this script shouldn't be running with root privileges (<.<)"
+		
 	# Get the command given
 	case ${ARGS[0]} in
 		
-		# Prepare the source and generate the .dsc file
-		prepare)
-		
-			# Check the number of arguments before going forward
-			[[ ${ARGS_NB} -ne 7 ]] && { error 'prepare_options' ; return $? ; }
+		# Build the client
+		build)
 			
-			# Get the arguments
-			for i in 1 3 5
-			do
-				case ${ARGS[$i]} in
-					--conf)
-						conf_file=${ARGS[$(($i + 1))]}
-						;;
-					--dist)
-						distribution=${ARGS[$(($i + 1))]}
-						;;
-					--arch)
-						architecture=${ARGS[$(($i + 1))]}
-						;;
-					*)
-						error unknown_option "${ARGS[$i]}"
-						return $?
-						;;
-				esac
-			done
+			# Get the configuration...
+			case ${ARGS[1]} in
 			
-			# Source the configuration file exists
-			[[ -e ${conf_file} ]] \
+				# ... based on the name of the Jenkins job
+				auto)
+					
+					# Get the information from the Jenkins job name
+					config=$(get_config_auto)
+					dist=$(echo $config | cut -d ',' -f 2)
+					arch=$(echo $config | cut -d ',' -f 3)
+					conf_file="./configs/$(echo $config | cut -d ',' -f 1)-${dist}.cfg"
+					;;
+				
+				# ... manually given as options
+				manual)
+					
+					# Check the number of arguments before going forward
+					[[ ${ARGS_NB} -ne 8 ]] && { error 'manual_options' ; return $? ; }
+			
+					# Get the arguments
+					for i in 2 4 6
+					do
+						case ${ARGS[$i]} in
+							--conf)
+								conf_file=${ARGS[$(($i + 1))]}
+								;;
+							--dist)
+								dist=${ARGS[$(($i + 1))]}
+								;;
+							--arch)
+								arch=${ARGS[$(($i + 1))]}
+								;;
+							*)
+								error 'unknown_option' "${ARGS[$i]}"
+								return $?
+								;;
+						esac
+					done
+					;;
+				
+				# Wrong argument
+				*)
+					error 'unknown_argument' "${ARGS[1]}" 'build'
+					return $?
+					;;
+			esac
+			
+			# Source the appropriate configuration file if it exists
+			[[ -e "$conf_file" ]] \
 				&& source ${conf_file} \
 				|| { error 'conf_file' "${conf_file}" ; return $? ; }
 			
-			# Warn if this is run as root
-			[[ $(id -u) -eq 0 ]] && echo "WARNING: You shouldn't run ./${PROGNAME} ${ARGS[0]} with root privileges."
+			# Tests the presence of the source folder
+			[[ -d "${CLIENT_NAME,,}_src" ]] \
+				|| { error 'source_folder' "${CLIENT_NAME,,}_src" ; return $? ;}
 			
-			# Get the source and generate the .dsc
-			get_source
+			# Get the client version
 			client_version=$(get_version)
-			prepare_source "$client_version" "$distribution"
 			
-			# Save the configuration for the next step
-			cat <<- EOF > .toxdeb_prepared
-			version=${client_version}
-			configuration=${conf_file}
-			distribution=${distribution}
-			architecture=${architecture}
-			EOF
+			# Prepares the folders
+			prepare_folders "$client_version" || return $?
 			
-			# Instructions for the next step
-			echo 'INFO: The sources are ready to be built.'
-			echo "INFO: Run ./${PROGNAME} build as root to build ${CLIENT_NAME}."
-			;;
-			
-		# Build the clients with pbuilder
-		build)
-		
-			# Check the number of arguments before going forward
-			[[ ${ARGS_NB} -ne 1 ]] && { error 'build_options' ; return $? ; }
-			
-			# Build should be run as root
-			[[ $(id -u) -ne 0 ]] && { error 'build_root' ; return $? ; }
-			
-			# Gets the parameters previously configured
-			client_version=$(grep 'version' .toxdeb_prepared | sed 's/version=//')
-			conf_file=$(grep 'configuration' .toxdeb_prepared | sed 's/configuration=//')
-			distribution=$(grep 'distribution' .toxdeb_prepared | sed 's/distribution=//')
-			architecture=$(grep 'architecture' .toxdeb_prepared | sed 's/architecture=//')
-			
-			# Source the configuration file
-			source ${conf_file}
-			
-			# Check if the pbuilder chroot exists...
-			[[ -e "/var/cache/pbuilder/${distribution}-${architecture}.tgz" ]] \
-				|| { error 'chroot_nonexistent' "$distribution" "$architecture" ; return $? ; }
-			
-			# Info
-			echo "INFO: Now building ${CLIENT_NAME} ${client_version}!"
-			echo "INFO: Distribution: $distribution / Architecture: $architecture"
+			# Prepares the source to be built
+			prepare_source "$client_version" "$dist" || return $?
 			
 			# Launch the build process
-			build_client "$client_version" "$distribution" "$architecture"
-			
-			# Remove the save file
-			rm .toxdeb_prepared
+			build_client "$dist" "$arch" || return $?
 			;;
 			
 		# Displays the help
 		help)
 			help
+			return $?
 			;;
 		
 		# Well... at least you tried :P
 		*)
-			error 'unknown_operation' "${ARGS[0]}"
+			error 'unknown_argument' "${ARGS[0]}" "$PROGNAME"
+			return $?
 			;;
 	esac
 	
-	# Return the correct exit code
-	return $?
+	# If everything went well, SUCCESS!
+	return 0
 }
 
-# Get the source code from github
-get_source() {
+# Get the build information (for auto mode)
+get_config_auto() {
+
+	# Local variables
+	local client dist arch
+	local config
 	
-	# If the code has already been cloned, update
-	if [[ -d "${CLIENT_NAME,,}_src" ]]
-	then
-		cd "${CLIENT_NAME,,}_src"
-		git checkout "${BUILD_BRANCH}"
-		git pull
+	# Gets the client name
+	client=$(echo ${JOB_NAME} | cut -d '_' -f 1)
 	
-	# Else, download it from the git repo
-	else
-		git clone "${GIT_REPO}" "${CLIENT_NAME,,}_src"
-		cd "${CLIENT_NAME,,}_src"
-		git checkout "${BUILD_BRANCH}"
-	fi
+	# Gets the distribution
+	config=$(echo ${JOB_NAME} | grep -o 'wheezy\|jessie\|stretch\|sid\|trusty\|utopic\|vivid\|wily\|xenial')
+	[[ -n "$config" ]] \
+		&& dist="$config" \
+		|| { error 'auto_config' 'distribution' ; return $? ; }
 	
-	# Back to parent directory
-	cd ..
+	# Gets the architecture
+	config=$(echo ${JOB_NAME} | grep -o '_x86_\|_x86-64_')
+	case $config in
+		'_x86_')
+			arch='i386'
+			;;
+		'_x86-64_')
+			arch='amd64'
+			;;
+		*)
+			error 'auto_config' 'architecture'
+			return $?
+			;;
+	esac
+	
+	# Returns the parameters found
+	echo "${client,,},${dist},${arch}"
 	
 	return 0
 }
 
-# Gets the version of µTox
+# Gets the version of the client
 get_version() {
 
 	# Local variables
@@ -174,6 +180,23 @@ get_version() {
 	return 0
 }
 
+# Set up correctly the folders for dh_make
+prepare_folders() {
+
+	# Local variables
+	local client_version=$1
+	local parent_folder
+	
+	# Correctly rename the source folder (becoming the parent folder)
+	parent_folder="${CLIENT_NAME,,}-${client_version}"
+	mv "${CLIENT_NAME,,}_src" "${parent_folder}"
+	
+	# Convert the source as a .tar.gz archive
+	tar -zcf "${parent_folder}.tar.gz" "${parent_folder}"
+	
+	return 0
+}
+
 # Prepare the source for pbuilder, generating a .dsc file
 prepare_source() {
 
@@ -181,16 +204,10 @@ prepare_source() {
 	local client_version=$1
 	local distribution=$2
 	local parent_folder
+	local changelog_file changelog_footer
 	
-	# Clean the previous version of the program built if there is any
-	rm -rf $(find . -maxdepth 1 -name "${CLIENT_NAME,,}[-_][0-9]*")
-	
-	# Correctly rename the source folder (becoming the parent folder)
+	# Sets the parent_folder
 	parent_folder="${CLIENT_NAME,,}-${client_version}"
-	cp -r "${CLIENT_NAME,,}_src" "${parent_folder}"
-	
-	# Convert the source as a .tar.gz archive
-	tar -zcvf "${parent_folder}.tar.gz" "${parent_folder}"
 	
 	# Invoke dh_make to prepare the source
 	export DEBFULLNAME="${MAINT_NAME}"
@@ -220,7 +237,8 @@ prepare_source() {
 		-i debian/copyright
 	
 	# Add the manpage
-	cp $(find . -name ${CLIENT_NAME,,}.1) debian/
+	cp $(find . -name ${CLIENT_NAME,,}.1) debian/ \
+		|| echo 'WARNING: No manpage found!'
 	
 	# Set the menu file
 	cat <<- EOF > debian/menu
@@ -235,11 +253,36 @@ prepare_source() {
 	# Set the distribution
 	sed -i "1s/unstable/${distribution}/g" debian/changelog
 	
-	# Get the changelog from the author
-	echo 'YOUR INPUT IS NEEDED!'
-	echo "Please enter the changelog for this version of ${CLIENT_NAME}"
-	read -p 'Press enter to edit... '
-	dch -e
+	# Adds changelog from CHANGELOG.md
+	changelog_file=$(find . -name 'CHANGELOG.md')
+	
+	if [[ -n "$changelog_file" ]]
+	then
+	
+		# Saves the footer
+		changelog_footer=$(grep ' --' debian/changelog)
+		
+		# Delete all lines but keep the header
+		sed -i '1,2!d' debian/changelog
+		
+		# Inserts the changelog
+		awk '{if(match($0, /## v?([0-9]\.){2}[0-9].*/) != 0){i++} ; if(match($0, /#### .*/) != 0){j++} ; if(i != 2 && j >= 1){print} else if(i == 2){exit}}' ${changelog_file} | \
+		sed -e '/^</d' \
+			-e '/^$/d' \
+			-e 's/  \+\([^*]\)/ \1/g' \
+			-e 's/[*]\{2,\}//g' \
+			-e 's/#### \(.*\)/\n  * \1:/' \
+			-e 's/ (\[[0-9a-f]\{8\}\].*//' \
+			-e 's/^\( *\)[*]/    \1*/' \
+		>> debian/changelog
+		
+		# Add the footer
+		echo -e "\n${changelog_footer}" >> debian/changelog
+		
+	else
+		echo 'WARNING: No CHANGELOG.md found! No changelog will be produced.'
+		sed -i 's/^  [*].*/  * No changelog available./g' debian/changelog
+	fi
 	
 	# Call dpkg-source to generate the .dsc file
 	dpkg-source -b .
@@ -254,20 +297,18 @@ prepare_source() {
 build_client() {
 
 	# Local variables
-	local client_version=$1
-	local distribution=$2
-	local architecture=$3
+	local distribution=$1
+	local architecture=$2
+	
+	# Set the environment variables
+	export DIST=${distribution}
+	export ARCH=${architecture}
+	export REPOBASE=$PWD
 	
 	# Call pbuilder
-	pbuilder --build --basetgz "/var/cache/pbuilder/${distribution}-${architecture}.tgz" *.dsc
+	sudo pbuilder --update --override-config
+	sudo pbuilder build --buildresult $PWD *.dsc
 	[[ $? -ne 0 ]] && { error 'pbuilder_failed' ; return $?; }
-	
-	# Save the result in a folder
-	mkdir "${CLIENT_NAME,,}_build_v${client_version}_${target}_${architecture}"
-	mv /var/cache/pbuilder/result/* "${CLIENT_NAME,,}_build_v${client_version}_${target}_${architecture}"
-	
-	# Final cleanup
-	rm -rf $(find . -maxdepth 1 -name "${CLIENT_NAME,,}[-_][0-9]*")
 	
 	return 0
 }
@@ -287,24 +328,22 @@ error() {
 		conf_file)
 			echo "The configuration file ($2) given has argument doesn't exist." >&2
 			;;
-		prepare_options)
-			echo 'The operation "prepare" expects 3 options with their respective values.' >&2
+		source_folder)
+			echo "The source folder $2 was expected in the workspace but it is missing!" >&2
+			;;
+		auto_config)
+			echo "Couldn't find automatically the $2 for this job." >&2
+			echo "Please rename your job or use the manual option." >&2
+			;;
+		manual_options)
+			echo 'The operation "build manual" expects 3 options with their respective values.' >&2
 			echo "Use \"${PROGNAME} help\" to get further help." >&2
 			;;
-		build_options)
-			echo 'The operation "build" does not expect any argument.' >&2
+		unknown_argument)
+			echo "Unknown argument $2 for $3." >&2
 			;;
 		unknown_option)
 			echo "Unknown option $2 for operation \"prepare\"." >&2
-			;;
-		chroot_nonexistent)
-			echo "Nonexistent base .tgz for distribution $2 and architecture $3!" >&2
-			;;
-		unknown_operation)
-			echo "Unknown operation $2." >&2
-			;;
-		build_root)
-			echo "${PROGNAME} requires root privileges to run the operation \"build\"." >&2
 			;;
 		pbuilder_failed)
 			echo "Pbuilder failed. Consult the logs for more information." >&2
@@ -321,22 +360,21 @@ error() {
 help() {
 
 	cat <<- EOF
-	Usage: ${PROGNAME} [ prepare <options> | build | help ]
+	Usage: ${PROGNAME} [ build [ manual <options> | auto ] | help ]
 	
 	Operations:
-	  prepare: Gets the latest sources of the repository "GIT_REPO" and prepares the .dsc file.
-	  build: Uses pbuilder to build the Tox client from the .dsc file generated.
+	  build manual: Builds the Tox client based on the options given.
+	  build auto: Builds the Tox client based on the Jenkins job name.
 	  help: Displays this help.
 	
-	Options of "prepare":
+	Options of "build manual" (mandatory):
 	  --conf: Path to the configuration file.
 	  --dist: Sets the target distribution.
 	  --arch: Sets the target architecture.
 	
-	Note that "prepare" should be run as an unpriviledged user, whereas "build" should be run as root.
-	
-	One will probably run: ./${PROGNAME} prepare --distribution jessie --architecture amd64 && sudo ./${PROGNAME} build 
-	Please make sure that the configuration is correct before running this tool.
+	One will probably run: ./${PROGNAME} build auto for a job such as "uTox_pkg_linux_deb_shared_jessie_x86-64_nightly_release"
+	In case of manual configuration: ./${PROGNAME} build --dist jessie --arch amd64 --conf ./configs/utox-jessie.cfg
+	Please make sure that the configuration file is correct before running this tool.
 	EOF
 
 	return 0
